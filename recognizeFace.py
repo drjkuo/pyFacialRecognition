016d593aeafba5a648b9912660e2a510864dd7e8
KUO <drjkuo@gmail.com>
2017-12-09 13:25:45 -0800

    fix module reference

--- recognizeFace.py
+++ recognizeFace.py
@@ -2,7 +2,8 @@ import cv2,sys
 faceClassifier=cv2.CascadeClassifier('haarcascade_frontalface_default.xml') # the HAAR cascade file, which contains the machine learned data for face detection
 objImage=cv2.imread(sys.argv[1]) # use cv2 to load image file
 cvtImage=cv2.cvtColor(objImage,cv2.COLOR_BGR2GRAY) # convert the image to gray scale
-foundFaces=faceClassifier.detectMultiScale(cvtImage,scaleFactor=1.3,minNeighbors=9,minSize=(50,50),flags = cv2.cv.CV_HAAR_SCALE_IMAGE) # to detect faces
+# foundFaces=faceClassifier.detectMultiScale(cvtImage,scaleFactor=1.3,minNeighbors=9,minSize=(50,50),flags = cv2.cv.CV_HAAR_SCALE_IMAGE) # to detect faces
+foundFaces=faceClassifier.detectMultiScale(cvtImage,scaleFactor=1.3,minNeighbors=9,minSize=(50,50),flags = cv2.CASCADE_SCALE_IMAGE) # to detect faces
 print(" There are {} faces in the input image".format(len(foundFaces)))
 for (x,y,w,h) in foundFaces:# to iterate each faces founded
 	cv2.rectangle(objImage,(x,y),(x+w,y+h),(0,0,255),2)
